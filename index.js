3491f4f9cbd51710ba044fc14420d859f04b7422
Ilari Kajaste <ilari@valaa.com>
2021-09-13 12:42:32 +0300

    feat: most of export feature, wide search if direct fails, expansion search terms include root name

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 fs = require('fs');
 const { Client } = require("@notionhq/client");
 const { makeConsoleLogger } = require('@notionhq/client/build/src/logging');
+const { exit } = require('process');
+const { start } = require('repl');
 
 const verbose = true;
 
@@ -25,8 +27,15 @@ const notion = new Client({
   auth: secret_token,
 });
 
-async function bgg_searchGame(queryText) {
-  return await bgg('search', {'query': queryText, 'exact': 1, 'type': 'boardgame,boardgameexpansion,boardgameaccessory'});
+async function bgg_searchGame(queryText, exact=false) {
+  // Apparently BGG api can't handle ":" even if encoded as "%3A"
+  const qs = queryText.replace(":", " ");
+  let res = await bgg('search', {'query': qs, 'exact': 1, 'type': 'boardgame,boardgameexpansion,boardgameaccessory'});
+  if (exact) return res;
+  if (res.items && res.items.total && res.items.total > 0) return res;
+
+  res = await bgg('search', {'query': qs, 'exact': 0, 'type': 'boardgame,boardgameexpansion,boardgameaccessory'});
+  return res;
 }
 
 // Convert and append bgg data into a more friendly-to-use internal format
@@ -85,7 +94,7 @@ function bgg_processItem(item) {
 
 async function bgg_multiGameDetails(gameIds) {
   res = await bgg('thing', {'id': gameIds.join(","), "versions": 0, "videos": 0, "stats": 0, "comments": 0, "ratingcomments": 0});
-  nitems = [];
+  const nitems = [];
   for (const item of res.items.item) {
     nitems.push(bgg_processItem(item));
   }
@@ -98,7 +107,8 @@ async function bgg_gameDetails(gameId) {
 }
 
 
-function groupString(items, max=1) {
+function groupString(items, max=1, placeholder="") {
+  if (!items || !Array.isArray(items)) return placeholder;
   const c = items;
   let str = items.slice(0, max).join(", ");
   if (c > max) str = str+' + '+(publisherCount-3)+" more";
@@ -106,13 +116,17 @@ function groupString(items, max=1) {
 }
 
 function bgg_detailString(det) {
-  const publisherString = groupString(det.details.boardgamepublisher, 3);
-  const designerString = groupString(det.details.boardgamedesigner, 3);
+  const publisherString = groupString(det.details.boardgamepublisher, 3, "?");
+  const designerString = groupString(det.details.boardgamedesigner, 3, "?");
   return "["+det.id+": "+det.primaryName+" ("+det.yearpublished+") "+det.minplayers+"-"+det.maxplayers+"ply "+det.playingtime+"min by "+designerString+" from "+publisherString+"]";
 }
 
 function dumpj(data) {
   console.log(JSON.stringify(data,null,2));
+}
+
+function notion_item_debug(notionData) {
+  return notionData.properties["DEBUG_THIS"] && notionData.properties["DEBUG_THIS"].checkbox && notionData.properties["DEBUG_THIS"].checkbox == true;
 }
 
 class NotionItem {
@@ -124,6 +138,20 @@ class NotionItem {
     this.itemData = itemData;
   }
 
+  setContainer(container) {
+    this.container = container;
+  }
+
+  doDebug() {
+    if (this.debug) {
+      //dumpj(queueItem.notion_data);
+      dumpj(this.data.properties["Display name"]);
+      //dumpj(this.data.properties["Expansion for"]);
+      dumpj(this.data.properties["Expanded by"]);
+      //dumpj(this.data.properties["Root game name"]);
+    }
+  }
+
   hasName() {
     const gameTitleObject = this.itemData.properties["Name"].title;
     if (gameTitleObject.length === 0) return false;
@@ -131,12 +159,74 @@ class NotionItem {
     if (name == '' || name == 'undefined') return false;
     return true;
   }
+  get lautapeliopas_url() {
+    const propertyData = this.itemData.properties["Lautapeliopas"];
+    if (!propertyData) return false;
+    return propertyData.url;
+  }
+  get debug() {
+    return notion_item_debug(this.itemData);
+  }
+  get category() {
+    const propertyData = this.itemData.properties["Game category"];
+    if (!propertyData) return "default";
+    return propertyData.select.name;
+  }
+  get alternateNames() {
+    const propertyData = this.itemData.properties["Alternate names"];
+    if (!propertyData) return null;
+    return propertyData.rich_text.map(i => i.plain_text).join("");
+  }
+  get displayName() {
+    const propertyData = this.itemData.properties["Display name"];
+    if (!propertyData) return null;
+    return propertyData.formula.string;
+  }
   get name() {
     const gameTitleObject = this.itemData.properties["Name"].title;
     if (gameTitleObject.length > 0) {
       return gameTitleObject[0].plain_text;
     } else {
       return "";
+    }
+  }
+  isExpansion() {
+    const propertyData = this.itemData.properties["Expansion for"];
+    if (propertyData.relation.length > 0) return true;
+    return false;
+  }
+  hasExpansions() {
+    const propertyData = this.itemData.properties["Expanded by"];
+    if (propertyData.relation.length > 0) return true;
+    return false;
+  }
+  get expansions() {
+    const propertyData = this.itemData.properties["Expanded by"];
+    if (propertyData.relation.length == 0) return [];
+    const items = propertyData.relation.map(i => this.container.getItem(i.id));
+    return items;
+  }
+  get rootItem() {
+    const propertyData = this.itemData.properties["Expansion for"];
+    if (propertyData.relation.length == 0) return null;
+    const rootGameId = propertyData.relation[0].id;
+    console.log("root game id:", rootGameId);
+    const ri = this.container.getItem(rootGameId);
+    console.log("root game:", ri);
+    return ri;
+  }
+  get expansionRootName() {
+    const propertyData = this.itemData.properties["Root game name"];
+    if (propertyData.rollup.array.length == 0) return null;
+    return propertyData.rollup.array[0].title[0].plain_text;
+    //return this.rootItem ? this.rootItem.name : null;
+  }
+  get queryTitle() {
+    if (this.isExpansion()) {
+      const rootname = this.expansionRootName;
+      return rootname ? rootname+": "+this.name : this.name;
+    } else {
+      return this.name;
     }
   }
   get id() {
@@ -149,8 +239,15 @@ class NotionContainer {
 
   include(item) {
     const itemId = item.id;
+    item.container = this;
     if (!this.hasItem(item.id)) {
       this.items.push(item);
+    }
+    item.doDebug();
+  }
+  getItem(id) {
+    for (const item of this.items) {
+      if (item.id == id) return item;
     }
   }
   hasItem(id) {
@@ -198,19 +295,22 @@ async function doThings() {
             if (verbose) console.log("Already has potential matches, skipping");
             continue;
           }
-          if (verbose) console.log("Performing a BGG search for: "+gameTitle);
-          const bggResult = await bgg_searchGame(gameTitle);
-          //dumpj(bggResult);
+          queryString = game.queryTitle;
+          if (verbose) console.log("Performing a BGG search for: "+queryString);
+          let bggResult = await bgg_searchGame(queryString);
+          dumpj(bggResult);
           if (Array.isArray(bggResult.items.item)) {
             if (verbose) console.log("Found "+bggResult.items.total+" potential matches");
             const potentialIds = bggResult.items.item.map(item => item.id);
             const bggDetails = await bgg_multiGameDetails(potentialIds);
             const potentialStrings = bggDetails.map(det => bgg_detailString(det));
+            let finalPotentialString = potentialStrings.join("\n");
+            if (finalPotentialString.length > 2000) finalPotentialString = finalPotentialString.slice(0,1997) + "...";
             if (verbose) console.log("Updating potential matches to Notion...");
             await notion.pages.update({
               page_id: gameItem.id,
               properties: {
-                bgg_potential_matches: { rich_text: [ { type: "text", text: { content: potentialStrings.join("\n") } } ] },
+                bgg_potential_matches: { rich_text: [ { type: "text", text: { content: finalPotentialString } } ] },
                 //api_sync_notes:  { rich_text: [ { type: "text", text: { content: "Synced. "+Math.floor(Math.random()*1000) } } ] },
                 api_latest_sync: { date: { start: currentTime.toISOString() } }
               }
@@ -274,7 +374,12 @@ async function doThings() {
       }
     }
 
-    //dumpj(queueItem.notion_data);
+    if (notion_item_debug(queueItem.notion_data)) {
+      //dumpj(queueItem.notion_data);
+      //dumpj(queueItem.notion_data.properties["Display name"]);
+      //dumpj(queueItem.notion_data.properties["Expansion for"]);
+      //game = notionItems.getItem(queueItem.notion_id)
+    }
     if (queueItem.notion_data.data_complete && queueItem.notion_data.data_complete.checkbox == true) continue; // data marked as complete, skipping
     await sync("Players min", "minplayers", "number");
     await sync("Players max", "maxplayers", "number");
@@ -300,10 +405,66 @@ async function doThings() {
 
 }
 
+function exportTitle(item) {
+  let add = item.displayName;
+  if (item.lautapeliopas_url) {
+    add = '<a href="'+item.lautapeliopas_url+'">'+add+'</a>';
+  }
+  return add;
+}
+
 function doExport() {
   notionItems.sort((a,b) => a.name.toUpperCase() < b.name.toUpperCase() ? -1 : +1);
+  let out = "";
+  const outCat = {};
+  const knownCategories = ["default", "Party game", "Children's game"];
   for (const item of notionItems.items) {
-    console.log('-', item.name);
+    if (item.isExpansion()) continue;
+    let add = exportTitle(item) + "<br />\n";
+
+    const cat = knownCategories.includes(item.category) ? item.category : "default";
+    if (!outCat[cat]) outCat[cat] = [];
+    startLetter = item.name.slice(0,1);
+    if (item.hasExpansions()) {
+      const exps = item.expansions.sort((a,b) => a.name.toUpperCase() < b.name.toUpperCase() ? -1 : +1);
+      for (const exp of exps) {
+        add = add + '&nbsp;&nbsp;&nbsp;&nbsp;lisäosa: '+ exportTitle(exp) + "<br />\n";
+      }
+    }
+    outCat[cat].push({ add: add, name: item.name, id: item.id, startLetter: startLetter });
+
+  }
+
+  for (const category of knownCategories) {
+    let prevStartLetter = "";
+    out += "\n\n<!-- Category: "+category+" -->\n<p>\n\n"
+    if (!outCat[category]) continue;
+    for (const item of outCat[category]) {
+      if (prevStartLetter != item.startLetter) {
+        out += "\n<!-- "+item.startLetter+" -->\n\n<br/>\n";
+        prevStartLetter = item.startLetter;
+      }
+      out += item.add;
+    }
+    out += "\n\n</p>\n\n"
+  }
+
+  try {
+    let content = fs.readFileSync('export-src/pre');
+    out = content + out;
+  } catch (err) {
+    console.log("No export-src/pre found - ignoring");
+  }
+  try {
+    let content = fs.readFileSync('export-src/post');
+    out = out + content;
+  } catch (err) {
+    console.log("No export-src/post found - ignoring");
+  }
+  try {
+    fs.writeFileSync("export/index.html", out);
+  } catch (err) {
+    console.warn("Failed to write to export/index.html");
   }
 }
 
